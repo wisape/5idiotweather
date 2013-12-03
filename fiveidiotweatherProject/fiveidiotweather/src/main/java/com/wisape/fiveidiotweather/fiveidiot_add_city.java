e5870f4d4cf97b6e9bf3f58490903ddb86ca5076
Guoying Zhang <Guoying.Zhang@csr.com>
2013-12-03 10:44:36 +0800

    5sha:add basic citylist activity
    Signed-off-by: wisape <wisape@163.com>

--- fiveidiotweatherProject/fiveidiotweather/src/main/java/com/wisape/fiveidiotweather/fiveidiot_add_city.java
+++ fiveidiotweatherProject/fiveidiotweather/src/main/java/com/wisape/fiveidiotweather/fiveidiot_add_city.java
@@ -2,15 +2,25 @@ package com.wisape.fiveidiotweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 /**
  * Created by gz09 on 13-12-2.
  */
-public class fiveidiot_add_city extends Activity {
-    fiveidiot_cityids_db cityids_db;
+public class fiveidiot_add_city extends Activity implements View.OnClickListener {
+    private fiveidiot_cityids_db cityids_db;
+    private ListView listView;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_city_activity);
         cityids_db = new fiveidiot_cityids_db(getApplicationContext());
+        listView = (ListView) findViewById(R.id.add_city);
+        listView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
