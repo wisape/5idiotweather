03efff5a09e8183e473db2650b9bb5f01094a6cb
Guoying Zhang <Guoying.Zhang@csr.com>
2013-12-03 18:19:38 +0800

    5sha:add the widget clicked open activity
    Signed-off-by: wisape <wisape@163.com>

--- fiveidiotweatherProject/fiveidiotweather/src/main/java/com/wisape/fiveidiotweather/fiveidiot_widget_provider.java
+++ fiveidiotweatherProject/fiveidiotweather/src/main/java/com/wisape/fiveidiotweather/fiveidiot_widget_provider.java
@@ -1,8 +1,10 @@
 package com.wisape.fiveidiotweather;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -15,8 +17,10 @@ public class fiveidiot_widget_provider extends AppWidgetProvider {
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
-
+            Intent intent = new Intent(context, fiveidiot.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.today_brief);
+            views.setOnClickPendingIntent(R.id.today_con, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
