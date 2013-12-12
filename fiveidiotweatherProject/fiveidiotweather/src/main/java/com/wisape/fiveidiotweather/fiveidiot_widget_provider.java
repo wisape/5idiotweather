1d226c9877f640055574cdb7cdb708be6e162dd1
Guoying Zhang <Guoying.Zhang@csr.com>
2013-12-12 19:22:05 +0800

    5sha:add the update of the widget
    Signed-off-by: wisape <wisape@163.com>

--- fiveidiotweatherProject/fiveidiotweather/src/main/java/com/wisape/fiveidiotweather/fiveidiot_widget_provider.java
+++ fiveidiotweatherProject/fiveidiotweather/src/main/java/com/wisape/fiveidiotweather/fiveidiot_widget_provider.java
@@ -5,12 +5,17 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import java.util.Map;
 
 /**
  * Created by wisape on 13-12-3.
  */
 public class fiveidiot_widget_provider extends AppWidgetProvider {
+    private fiveidiotreaddb readdb;
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
 
@@ -20,10 +25,29 @@ public class fiveidiot_widget_provider extends AppWidgetProvider {
             Intent intent = new Intent(context, fiveidiot.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget4x1);
+            updateViews(context, views);
             views.setOnClickPendingIntent(R.id.today_con, pendingIntent);
-
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+    }
+
+    private String setDefault(Object str1, String str2) {
+        if (str1 == null)
+            return str2;
+        return str1.toString();
+    }
+
+    private void updateViews(Context context, RemoteViews views) {
+        readdb = new fiveidiotreaddb(context);
+        Map<String, Object> data_map = readdb.getTodayBriefMapData("上海");
+        if (data_map.get("city") == null)
+            return;
+        views.setTextViewText(R.id.city, data_map.get("city").toString());
+        views.setTextViewText(R.id.temp,setDefault(data_map.get("temp0"),"1"));
+        views.setTextViewText(R.id.weather,setDefault(data_map.get("weather0"),"1"));
+        views.setTextViewText(R.id.wind,setDefault(data_map.get("wind"),"1"));
+        views.setTextViewText(R.id.nowtemp,setDefault(data_map.get("nowtemp"),"1") + "℃");
+        views.setImageViewResource(R.id.today_image, R.drawable.cloud);
     }
 }
